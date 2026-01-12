0570ea6f0e4d0175537e2949c9216f65121b0dd4
Gokul Adethya <frozenwolf2003.com@gmail.com>
2026-01-12 01:40:21 -0800

    test benchmarks

--- generation/train_combined.py
+++ generation/train_combined.py
@@ -312,7 +312,7 @@ if __name__ == "__main__":
                     
                 if matched_unsup is not None:
                     orthogonal_loss = torch.cosine_similarity(concepts, matched_unsup, dim=-1).mean() ## TODO: check shape
-                val_losses["val_orthogonal_loss"].append(orthogonal_loss.detach().cpu().numpy())
+                    val_losses["val_orthogonal_loss"].append(orthogonal_loss.detach().cpu().numpy())
                 
                 neg_entropy_loss = torch.sum(p * torch.log(p), dim=-1).mean()
                 reg = elastic_net_penalty(cbl.fc.weight[:, :len(concept_set)])
