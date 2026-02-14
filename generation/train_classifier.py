13f81ccb5514c56d289bf484fef415751749ce21
Gokul Adethya <frozenwolf2003.com@gmail.com>
2026-02-13 23:50:16 -0800

    feat:no val for steer train

--- generation/train_classifier.py
+++ generation/train_classifier.py
@@ -1,19 +1,25 @@
 import argparse
 import os
+import gc
 import torch
+import torch.nn.functional as F
 import numpy as np
-from transformers import RobertaTokenizerFast, RobertaModel
-from datasets import load_dataset
+from transformers import RobertaTokenizerFast, RobertaModel, GPT2TokenizerFast, GPT2Model
+from datasets import load_dataset, concatenate_datasets
 import config as CFG
 from modules import Roberta_classifier
 import wandb
+from langdetect import detect
+import copy
 
 parser = argparse.ArgumentParser()
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 parser.add_argument("--dataset", type=str, default="SetFit/sst2")
 parser.add_argument("--batch_size", type=int, default=16)
 parser.add_argument("--max_length", type=int, default=100)
 parser.add_argument("--num_workers", type=int, default=0)
+
 
 class ClassificationDataset(torch.utils.data.Dataset):
     def __init__(self, encode_roberta):
@@ -21,6 +27,7 @@ class ClassificationDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
         t = {key: torch.tensor(values[idx]) for key, values in self.encode_roberta.items()}
+
         return t
 
     def __len__(self):
@@ -44,33 +51,44 @@ if __name__ == "__main__":
 
     print("loading data...")
     train_dataset = load_dataset(args.dataset, split='train')
+    print("tokenizing...")
 
     if args.dataset == 'ag_news':
         def replace_bad_string(example):
-            example["text"] = example["text"].replace("#36;", "").replace("#39;", "'")
+            example["text"] = example["text"].replace("#36;", "")
+            example["text"] = example["text"].replace("#39;", "'")
             return example
         train_dataset = train_dataset.map(replace_bad_string)
 
     tokenizer = RobertaTokenizerFast.from_pretrained('roberta-base')
-    def tokenize_fn(e):
-        return tokenizer(e[CFG.example_name[args.dataset]], padding=True, truncation=True, max_length=args.max_length)
 
-    print("tokenizing...")
-    encoded_train_dataset = train_dataset.map(tokenize_fn, batched=True, batch_size=len(train_dataset))
+    encoded_train_dataset = train_dataset.map(
+        lambda e: tokenizer(e[CFG.example_name[args.dataset]], padding=True, truncation=True,
+                            max_length=args.max_length), batched=True,
+        batch_size=len(train_dataset))
+    encoded_train_dataset = encoded_train_dataset.remove_columns([CFG.example_name[args.dataset]])
+    if args.dataset == 'SetFit/sst2':
+        encoded_train_dataset = encoded_train_dataset.remove_columns(['label_text'])
+    if args.dataset == 'dbpedia_14':
+        encoded_train_dataset = encoded_train_dataset.remove_columns(['title'])
+    encoded_train_dataset = encoded_train_dataset[:len(encoded_train_dataset)]
 
-    encoded_train_dataset.remove_columns([CFG.example_name[args.dataset]])
-    if args.dataset == 'SetFit/sst2': encoded_train_dataset.remove_columns(['label_text'])
-    if args.dataset == 'dbpedia_14': encoded_train_dataset.remove_columns(['title'])
-
-    print("creating loaders...")
+    print("creating loader...")
     train_loader = build_loaders(encoded_train_dataset, mode="train")
 
     concept_set = CFG.concepts_from_labels[args.dataset]
+    print("concept len: ", len(concept_set))
+
+    print("preparing classifier...")
     classifier = Roberta_classifier(len(concept_set)).to(device)
     optimizer = torch.optim.Adam(classifier.parameters(), lr=5e-6)
 
+
     print("start training...")
+    best_loss = float('inf')
+
     epochs = CFG.epoch[args.dataset]
+
     for e in range(epochs):
         print("Epoch ", e+1, ":")
         classifier.train()
